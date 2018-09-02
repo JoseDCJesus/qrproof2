98eb98b5b09ce7675c20779e5c752d3b84f3f44d
josejesus <josdcjesus@gmail.com>
2018-09-02 23:34:35 +0100

    My Django Girls app, first commit

--- get_loc/models.py
+++ get_loc/models.py
@@ -14,5 +14,8 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+    
+
+
 
 
--- get_loc/views.py
+++ get_loc/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.contrib.gis.geoip2 import GeoIP2
 from django.template import  RequestContext
+from geopy.geocoders import Nominatim
+import pygeoip
+
+
+from .models import Product
 
 # Create your views here.
 
@@ -21,10 +26,24 @@ def check(request, product_id):
 
     ip = get_client_ip(request)
 
-    return HttpResponse("We are analizing your request. Please wait a moment. This process will use your geographic location. Your ip is" + str(ip))
+    try:
+        product = Product.objects.get(pk=product_id)
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist ahaahah")
 
-    g = GeoIP2()
-    city = g.city(ip)
+    geolocator = Nominatim(user_agent="specify_your_app_name_here")
+    location = geolocator.geocode(product.sale_place)
+
+    return HttpResponse("We are analizing your request. Please wait a moment. This process will use your geographic location. Your ip is" + str(ip))
+    g = pygeoip.GeoIP('GeoIPCity.dat')
+
+    city_lat = g.record_by_addr(str(ip))['latitude']
+    city_log = g.record_by_addr(str(ip))['longitude']
+
+    if ( (abs(city_lat - location.latitude) <= 1) and ( abs(city_log - location.longitude) <= 0.3 ) ):
+        return Httpresponse( "O produto " + str(product.product_name) + " é autêntico  e originário de " + str(product.product_origin) )
+    else:
+        return HttpResponse("O produto é falsificado ")
 
     return HttpResponse("Teste cidade " + city)
 
