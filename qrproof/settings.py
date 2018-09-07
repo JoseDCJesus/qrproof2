7e901698f9853a41fd6b6a4c0649d2d1a2c10e5f
josejesus <josdcjesus@gmail.com>
2018-09-07 22:03:49 +0100

    My Django Girls app, first commit

--- qrproof/settings.py
+++ qrproof/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = '/home/JosDCJesus/josdcjesus.pythonanywhere.com/static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 #STATIC_ROOT = 'home/josejesus/Desktop/QRP/qrproof/'
 
 #GEOIP_PATH ='/home/josejesus/Desktop/QRProof/qrproof/geoip2_lib/'
