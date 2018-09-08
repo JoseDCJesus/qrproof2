9f6e040af9b464f54b8b0c47e6821ff2adefa018
josejesus <josdcjesus@gmail.com>
2018-09-08 13:10:27 +0100

    My Django Girls app, first commit

--- qrproof/settings.py
+++ qrproof/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = '/home/JosDCJesus/josdcjesus.pythonanywhere.com'
+STATIC_ROOT = '/home/JosDCJesus/josdcjesus.pythonanywhere.com/assets'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
